84da1b39dc3654198b200baad71684d89f95fd4e
maxiamikel <maxloversist@gmail.com>
2024-01-30 16:20:28 -0300

    Implementar el ProductoController, el template create, show

---
+++ src/main/java/com/maxi/ecommerce/controllers/ProductoController.java
@@ -0,0 +1,20 @@
+package com.maxi.ecommerce.controllers;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+
+@Controller
+@RequestMapping("/productos")
+public class ProductoController {
+
+    @GetMapping("")
+    public String show() {
+        return "productos/show";
+    }
+
+    @GetMapping("/create")
+    public String create() {
+        return "/productos/create";
+    }
+}
