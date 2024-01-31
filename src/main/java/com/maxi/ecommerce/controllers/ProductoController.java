138f86680a3d26ca221f64e5ac242aa0b9386e25
maxiamikel <maxloversist@gmail.com>
2024-01-31 13:30:05 -0300

    Impementar la vista edit y funcionalidd para eliminar un registro de producto

--- src/main/java/com/maxi/ecommerce/controllers/ProductoController.java
+++ src/main/java/com/maxi/ecommerce/controllers/ProductoController.java
@@ -1,5 +1,7 @@
 package com.maxi.ecommerce.controllers;
 
+import java.util.Optional;
+
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.annotation.Autowired;
@@ -8,6 +10,7 @@ import org.springframework.data.domain.Pageable;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestParam;
@@ -28,7 +31,7 @@ public class ProductoController {
     @GetMapping("")
     public String show(Model model,
             @RequestParam(name = "pageNumber", required = false, defaultValue = "0") int pageNumber,
-            @RequestParam(name = "pageSize", required = false, defaultValue = "2") int pageSize) {
+            @RequestParam(name = "pageSize", required = false, defaultValue = "3") int pageSize) {
         Pageable page = PageRequest.of(pageNumber, pageSize);
         model.addAttribute("productos", productoService.findAll(page));
         return "productos/show";
@@ -46,6 +49,29 @@ public class ProductoController {
         LOGGER.info("Nuevo producto {}", producto);
         producto.setUsuario(usuario);
         productoService.save(producto);
+        return "redirect:/productos";
+    }
+
+    @GetMapping("/edit/{id}")
+    public String edit(@PathVariable Integer id, Model model) {
+        Producto producto = new Producto();
+        Optional<Producto> optionalProducto = productoService.get(id);
+        producto = optionalProducto.get();
+        model.addAttribute("producto", producto);
+        LOGGER.info("Producto encontrado {}", producto);
+
+        return "/productos/edit";
+    }
+
+    @PostMapping("/update")
+    public String update(Producto producto) {
+        productoService.update(producto);
+        return "redirect:/productos";
+    }
+
+    @GetMapping("/delete/{id}")
+    public String delete(@PathVariable Integer id) {
+        productoService.delete(id);
         return "redirect:/productos";
     }
 
