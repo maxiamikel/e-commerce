a52075d5e3222f0dd3ffd23cb1a8abd174a7264a
maxiamikel <maxloversist@gmail.com>
2024-02-01 08:51:01 -0300

    Implementar la vista (UI) de detalle_producto y sus funcionalidades

--- src/main/java/com/maxi/ecommerce/controllers/HomeController.java
+++ src/main/java/com/maxi/ecommerce/controllers/HomeController.java
@@ -1,14 +1,18 @@
 package com.maxi.ecommerce.controllers;
 
+import java.util.Optional;
+
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.data.domain.PageRequest;
 import org.springframework.data.domain.Pageable;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestParam;
 
+import com.maxi.ecommerce.models.Producto;
 import com.maxi.ecommerce.services.producto.ProductoServiceIMplementation;
 
 @Controller
@@ -16,15 +20,24 @@ import com.maxi.ecommerce.services.producto.ProductoServiceIMplementation;
 public class HomeController {
 
     @Autowired
-    private ProductoServiceIMplementation ProductoService;
+    private ProductoServiceIMplementation productoService;
 
     @GetMapping("/")
     public String home(Model model,
             @RequestParam(name = "pageNumber", required = false, defaultValue = "0") int pageNumber,
             @RequestParam(name = "pageSize", required = false, defaultValue = "8") int pageSize) {
         Pageable page = PageRequest.of(pageNumber, pageSize);
-        model.addAttribute("productos", ProductoService.findAll(page));
+        model.addAttribute("productos", productoService.findAll(page));
         return "usuario/home";
+    }
+
+    @GetMapping("/detalle/{id}")
+    public String detalleProducto(@PathVariable Integer id, Model model) {
+        Producto producto = new Producto();
+        Optional<Producto> productoDB = productoService.get(id);
+        producto = productoDB.get();
+        model.addAttribute("producto", producto);
+        return "usuario/detalle_producto";
     }
 
 }
